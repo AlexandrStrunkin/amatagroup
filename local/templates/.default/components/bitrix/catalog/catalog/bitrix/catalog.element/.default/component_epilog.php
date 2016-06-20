281ec8b86b685ce23d7b57d1ee739e1519450506
Mikhail <lm@webgk.ru>
2016-06-20 17:10:56 +0300

    Create last viewed products

--- local/templates/.default/components/bitrix/catalog/catalog/bitrix/catalog.element/.default/component_epilog.php
+++ local/templates/.default/components/bitrix/catalog/catalog/bitrix/catalog.element/.default/component_epilog.php
@@ -2,7 +2,13 @@
 /** @var array $templateData */
 /** @var @global CMain $APPLICATION */
 use Bitrix\Main\Loader;
+use \Bitrix\Catalog\CatalogViewedProductTable as CatalogViewedProductTable;
 global $APPLICATION;
+$user_id = CSaleBasket::GetBasketUserID(); // нужен именно ID юзера в корзине !
+if($user_id  > 0) {
+	$product_id = $arResult['ID'];
+	CatalogViewedProductTable::refresh($product_id, $user_id);
+}
 if (isset($templateData['TEMPLATE_THEME']))
 {
 	$APPLICATION->SetAdditionalCSS($templateData['TEMPLATE_THEME']);
