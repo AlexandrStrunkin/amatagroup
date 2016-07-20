0d5f716ced3f28a21405583af7f664167f2945a2
Mikhail <lm@webgk.ru>
2016-07-20 12:22:13 +0300

    Partners reviews template added

--- local/templates/amatagroup_index/header.php
+++ local/templates/amatagroup_index/header.php
@@ -428,76 +428,74 @@
                 <div class="jcarousel-wrapper">
                     <!--jcarousel-->
                     <div class="jcarousel">
-                        <ul>
-                            <li>
-                                <div class="reviesElement">
-                                    <p class="reviesCity">Екатеринбург</p>
-
-                                    <p class="reviewsTitle">Отличный магазин, лучшие цены</p>
-
-                                    <p class="reviewsText">Покупаю технику в этом магазине не первый раз. Нравится всё.
-                                        Выбор,
-                                        обслуживание.</p>
-
-                                    <div class="authorsBlock">
-                                        <p class="reviewsAuthor">Виктория Фролова</p>
-
-                                        <p>CEO, Детский Мир</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="reviesElement">
-                                    <p class="reviesCity">Екатеринбург</p>
-
-                                    <p class="reviewsTitle">Отличный магазин, лучшие цены</p>
-
-                                    <p class="reviewsText">Покупаю технику в этом магазине не первый раз. Нравится всё.
-                                        Выбор,
-                                        обслуживание.</p>
-
-                                    <div class="authorsBlock">
-                                        <p class="reviewsAuthor">Мария Егорова</p>
-
-                                        <p>Генеральный директор, Lapsi</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="reviesElement">
-                                    <p class="reviesCity">Екатеринбург</p>
-
-                                    <p class="reviewsTitle">Отличный магазин, лучшие цены</p>
-
-                                    <p class="reviewsText">Покупаю технику в этом магазине не первый раз. Нравится всё.
-                                        Выбор,
-                                        обслуживание.</p>
-
-                                    <div class="authorsBlock">
-                                        <p class="reviewsAuthor">Константин Федоров</p>
-
-                                        <p>Директор, Enter</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="reviesElement">
-                                    <p class="reviesCity">Екатеринбург</p>
-
-                                    <p class="reviewsTitle">Отличный магазин, лучшие цены</p>
-
-                                    <p class="reviewsText">Покупаю технику в этом магазине не первый раз. Нравится всё.
-                                        Выбор,
-                                        обслуживание.</p>
-
-                                    <div class="authorsBlock">
-                                        <p class="reviewsAuthor">Виктория Фролова</p>
-
-                                        <p>CEO, Детский Мир</p>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
+                    	<?$APPLICATION->IncludeComponent(
+							"bitrix:news.list", 
+							"partners_reviews", 
+							array(
+								"ACTIVE_DATE_FORMAT" => "d.m.Y",
+								"ADD_SECTIONS_CHAIN" => "N",
+								"AJAX_MODE" => "N",
+								"AJAX_OPTION_ADDITIONAL" => "",
+								"AJAX_OPTION_HISTORY" => "N",
+								"AJAX_OPTION_JUMP" => "N",
+								"AJAX_OPTION_STYLE" => "Y",
+								"CACHE_FILTER" => "N",
+								"CACHE_GROUPS" => "Y",
+								"CACHE_TIME" => "36000000",
+								"CACHE_TYPE" => "A",
+								"CHECK_DATES" => "Y",
+								"DETAIL_URL" => "",
+								"DISPLAY_BOTTOM_PAGER" => "N",
+								"DISPLAY_DATE" => "N",
+								"DISPLAY_NAME" => "Y",
+								"DISPLAY_PICTURE" => "N",
+								"DISPLAY_PREVIEW_TEXT" => "Y",
+								"DISPLAY_TOP_PAGER" => "N",
+								"FIELD_CODE" => array(
+									0 => "NAME",
+									1 => "PREVIEW_TEXT",
+									2 => "",
+								),
+								"FILTER_NAME" => "",
+								"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+								"IBLOCK_ID" => "16",
+								"IBLOCK_TYPE" => "services",
+								"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+								"INCLUDE_SUBSECTIONS" => "Y",
+								"MESSAGE_404" => "",
+								"NEWS_COUNT" => "20",
+								"PAGER_BASE_LINK_ENABLE" => "N",
+								"PAGER_DESC_NUMBERING" => "N",
+								"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+								"PAGER_SHOW_ALL" => "N",
+								"PAGER_SHOW_ALWAYS" => "N",
+								"PAGER_TEMPLATE" => ".default",
+								"PAGER_TITLE" => "Новости",
+								"PARENT_SECTION" => "",
+								"PARENT_SECTION_CODE" => "",
+								"PREVIEW_TRUNCATE_LEN" => "",
+								"PROPERTY_CODE" => array(
+									0 => "CITY",
+									1 => "POSITION",
+									2 => "AUTHOR",
+									3 => "COMPANY",
+									4 => "",
+								),
+								"SET_BROWSER_TITLE" => "N",
+								"SET_LAST_MODIFIED" => "N",
+								"SET_META_DESCRIPTION" => "N",
+								"SET_META_KEYWORDS" => "N",
+								"SET_STATUS_404" => "N",
+								"SET_TITLE" => "N",
+								"SHOW_404" => "N",
+								"SORT_BY1" => "ACTIVE_FROM",
+								"SORT_BY2" => "SORT",
+								"SORT_ORDER1" => "DESC",
+								"SORT_ORDER2" => "ASC",
+								"COMPONENT_TEMPLATE" => "partners_reviews"
+							),
+							false
+						);?>
                     </div>
                     <a href="" class="jcarousel-control-prev"></a>
                     <a href="" class="jcarousel-control-next"></a>
