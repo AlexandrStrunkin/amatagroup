b24de8b39f5ef8d689d246ed672ca041a48e1e89
Mikhail <lm@webgk.ru>
2016-07-20 12:52:34 +0300

    Small company reviews style fixes

--- local/templates/.default/components/bitrix/news.list/partners_reviews/template.php
+++ local/templates/.default/components/bitrix/news.list/partners_reviews/template.php
@@ -12,26 +12,34 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<ul>
-<? foreach ($arResult["ITEMS"] as $arItem) { ?>
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?>
-	<li id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-		<div class="reviesElement">
-			<p class="reviesCity"><?= $arItem["PROPERTIES"]["CITY"]["VALUE"] ?></p>
-
-            <p class="reviewsTitle"><?= $arItem["NAME"] ?></p>
-
-            <p class="reviewsText"><?= $arItem["PREVIEW_TEXT"] ?></p>
-
-            <div class="authorsBlock">
-                <p class="reviewsAuthor"><?= $arItem["PROPERTIES"]["AUTHOR"]["VALUE"] ?></p>
-
-                <p><?= $arItem["PROPERTIES"]["POSITION"]["VALUE"] ?>, <?= $arItem["PROPERTIES"]["COMPANY"]["VALUE"] ?></p>
-            </div>
-		</div>
-	</li>
-<? } ?>
-</ul>
+<div class="jcarousel-wrapper">
+	<div class="jcarousel">
+		<ul>
+		<? foreach ($arResult["ITEMS"] as $arItem) { ?>
+			<?
+			$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+			$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+			?>
+			<li id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+				<div class="reviesElement">
+					<p class="reviesCity"><?= $arItem["PROPERTIES"]["CITY"]["VALUE"] ?></p>
+		
+		            <p class="reviewsTitle"><?= $arItem["NAME"] ?></p>
+		
+		            <p class="reviewsText"><?= $arItem["PREVIEW_TEXT"] ?></p>
+		
+		            <div class="authorsBlock">
+		                <p class="reviewsAuthor"><?= $arItem["PROPERTIES"]["AUTHOR"]["VALUE"] ?></p>
+		
+		                <p><?= $arItem["PROPERTIES"]["POSITION"]["VALUE"] ?>, <?= $arItem["PROPERTIES"]["COMPANY"]["VALUE"] ?></p>
+		            </div>
+				</div>
+			</li>
+		<? } ?>
+		</ul>
+	</div>
+	<? if (count($arResult["ITEMS"]) > 4) { ?>
+		<a href="" class="jcarousel-control-prev"></a>
+		<a href="" class="jcarousel-control-next"></a>
+	<? } ?>
+</div>
