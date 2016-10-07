bf2dae7e665f56b6a90a27e8e325a7c7d3c77517
Serega <st@webgk.ru>
2016-10-07 11:32:58 +0300

    SUP-5086: edit filter in section catalog

--- local/templates/.default/components/bitrix/catalog/catalog/section.php
+++ local/templates/.default/components/bitrix/catalog/catalog/section.php
@@ -213,7 +213,7 @@
                         "SECTION_ID_VARIABLE" => $arParams["SECTION_ID_VARIABLE"],
                         "PRODUCT_QUANTITY_VARIABLE" => $arParams["PRODUCT_QUANTITY_VARIABLE"],
                         "PRODUCT_PROPS_VARIABLE" => $arParams["PRODUCT_PROPS_VARIABLE"],
-                        "FILTER_NAME" => 'arFilter',
+                        "FILTER_NAME" => $arParams["FILTER_NAME"],
                         "CACHE_TYPE" => $arParams["CACHE_TYPE"],
                         "CACHE_TIME" => $arParams["CACHE_TIME"],
                         "CACHE_FILTER" => $arParams["CACHE_FILTER"],
