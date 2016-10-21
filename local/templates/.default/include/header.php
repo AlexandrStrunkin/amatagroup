a2924990090a73c5af07bd12322e58809bd96196
Alexander <faster@webgk.ru>
2016-10-21 12:34:55 +0300

    SUP-5219: change styles of fixed head menu

--- local/templates/.default/include/header.php
+++ local/templates/.default/include/header.php
@@ -5,10 +5,44 @@
             <a href="/"><img src="<?=DEFAULT_TEMPLATE_PATH?>img/headMainLogo.png" alt=""/></a>
         </div>
 
-        <div class="top-menu-fixed-catalog top-menu-fixed-block">
-            <div class="secondLvlBlocks menuControle" >
-                <p><?=GetMessage("CATALOG")?></p>
+        <div class="top-menu-fixed-catalog top-menu-fixed-block">       
+            <div class="secondLvlBlocks top-menu-fixed-catalog-control" >
+                <p class="catalog-fixed-title"><?=GetMessage("CATALOG")?></p>
             </div>
+            
+            <?/*  пока не удалять до решения задачи с плавающей плашкой
+            <div class="fixed-catalog-menu">
+                <?$APPLICATION->IncludeComponent(
+                        "bitrix:catalog.section.list",
+                        "catalog_menu",
+                        array(
+                            "COMPONENT_TEMPLATE" => "catalog_menu",
+                            "IBLOCK_TYPE" => "1c_catalog",
+                            "IBLOCK_ID" => "5",
+                            "SECTION_ID" => "",
+                            "SECTION_CODE" => "",
+                            "COUNT_ELEMENTS" => "N",
+                            "TOP_DEPTH" => "3",
+                            "SECTION_FIELDS" => array(
+                                0 => "",
+                                1 => "",
+                            ),
+                            "SECTION_USER_FIELDS" => array(
+                                0 => "",
+                                1 => "",
+                            ),
+                            "VIEW_MODE" => "LIST",
+                            "SHOW_PARENT_NAME" => "Y",
+                            "SECTION_URL" => "",
+                            "CACHE_TYPE" => "A",
+                            "CACHE_TIME" => "86400",
+                            "CACHE_GROUPS" => "N",
+                            "ADD_SECTIONS_CHAIN" => "Y"
+                        ),
+                        false
+                    );?>
+            </div>
+            */?>
         </div>
 
         <div class="top-menu-fixed-block auth-block">
@@ -124,19 +158,19 @@
             </div>
 
             <div class="firstLvlBlocks linksBlock js-small-basket" id="fixed-linkBlock3">
-                    <?$APPLICATION->IncludeComponent(
-                            "bitrix:sale.basket.basket.small",
-                            "small_basket",
-                            array(
-                                "COMPONENT_TEMPLATE" => "small_basket",
-                                "PATH_TO_BASKET" => "/personal/cart/",
-                                "PATH_TO_ORDER" => "/personal/order/make/",
-                                "SHOW_DELAY" => "Y",
-                                "SHOW_NOTAVAIL" => "Y",
-                                "SHOW_SUBSCRIBE" => "Y"
-                            ),
-                            false
-                        );?>
+                <?$APPLICATION->IncludeComponent(
+                        "bitrix:sale.basket.basket.small",
+                        "small_basket",
+                        array(
+                            "COMPONENT_TEMPLATE" => "small_basket",
+                            "PATH_TO_BASKET" => "/personal/cart/",
+                            "PATH_TO_ORDER" => "/personal/order/make/",
+                            "SHOW_DELAY" => "Y",
+                            "SHOW_NOTAVAIL" => "Y",
+                            "SHOW_SUBSCRIBE" => "Y"
+                        ),
+                        false
+                    );?>
             </div>
         </div>
 
@@ -377,121 +411,121 @@
             );?>
 
         <?$APPLICATION->IncludeComponent(
-	"bitrix:catalog.section",
-	"slider_main",
-	array(
-		"TEMPLATE_THEME" => "blue",
-		"PRODUCT_DISPLAY_MODE" => "N",
-		"ADD_PICT_PROP" => "-",
-		"LABEL_PROP" => "-",
-		"OFFER_ADD_PICT_PROP" => "FILE",
-		"OFFER_TREE_PROPS" => array(
-			0 => "-",
-		),
-		"PRODUCT_SUBSCRIPTION" => "N",
-		"SHOW_DISCOUNT_PERCENT" => "N",
-		"SHOW_OLD_PRICE" => "N",
-		"SHOW_CLOSE_POPUP" => "Y",
-		"MESS_BTN_BUY" => "Купить",
-		"MESS_BTN_ADD_TO_BASKET" => "В корзину",
-		"MESS_BTN_SUBSCRIBE" => "Подписаться",
-		"MESS_BTN_DETAIL" => "Подробнее",
-		"MESS_NOT_AVAILABLE" => "Нет в наличии",
-		"AJAX_MODE" => "Y",
-		"SEF_MODE" => "N",
-		"IBLOCK_TYPE" => "services",
-		"IBLOCK_ID" => "29",
-		"SECTION_ID" => $_REQUEST["SECTION_ID"],
-		"SECTION_CODE" => "",
-		"SECTION_USER_FIELDS" => array(
-			0 => "",
-			1 => "",
-		),
-		"ELEMENT_SORT_FIELD" => "sort",
-		"ELEMENT_SORT_ORDER" => "asc",
-		"ELEMENT_SORT_FIELD2" => "name",
-		"ELEMENT_SORT_ORDER2" => "asc",
-		"FILTER_NAME" => "arrFilter",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"SHOW_ALL_WO_SECTION" => "Y",
-		"SECTION_URL" => "",
-		"DETAIL_URL" => "",
-		"BASKET_URL" => "/personal/basket.php",
-		"ACTION_VARIABLE" => "action",
-		"PRODUCT_ID_VARIABLE" => "id",
-		"PRODUCT_QUANTITY_VARIABLE" => "quantity",
-		"ADD_PROPERTIES_TO_BASKET" => "Y",
-		"PRODUCT_PROPS_VARIABLE" => "prop",
-		"PARTIAL_PRODUCT_PROPERTIES" => "N",
-		"SECTION_ID_VARIABLE" => "SECTION_ID",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"DISPLAY_COMPARE" => "N",
-		"SET_TITLE" => "Y",
-		"SET_BROWSER_TITLE" => "Y",
-		"BROWSER_TITLE" => "-",
-		"SET_META_KEYWORDS" => "Y",
-		"META_KEYWORDS" => "",
-		"SET_META_DESCRIPTION" => "Y",
-		"META_DESCRIPTION" => "",
-		"SET_LAST_MODIFIED" => "Y",
-		"USE_MAIN_ELEMENT_SECTION" => "Y",
-		"SET_STATUS_404" => "Y",
-		"PAGE_ELEMENT_COUNT" => "30",
-		"LINE_ELEMENT_COUNT" => "3",
-		"PROPERTY_CODE" => array(
-			0 => "SPECIAL_OFFER",
-			1 => "PRICE_BANNER",
-			2 => "",
-		),
-		"OFFERS_FIELD_CODE" => "",
-		"OFFERS_PROPERTY_CODE" => "",
-		"OFFERS_SORT_FIELD" => "sort",
-		"OFFERS_SORT_ORDER" => "asc",
-		"OFFERS_SORT_FIELD2" => "active_from",
-		"OFFERS_SORT_ORDER2" => "desc",
-		"OFFERS_LIMIT" => "5",
-		"BACKGROUND_IMAGE" => "-",
-		"PRICE_CODE" => array(
-		),
-		"USE_PRICE_COUNT" => "Y",
-		"SHOW_PRICE_COUNT" => "1",
-		"PRICE_VAT_INCLUDE" => "Y",
-		"PRODUCT_PROPERTIES" => array(
-		),
-		"USE_PRODUCT_QUANTITY" => "Y",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "36000000",
-		"CACHE_FILTER" => "Y",
-		"CACHE_GROUPS" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"PAGER_TITLE" => "Товары",
-		"PAGER_SHOW_ALWAYS" => "Y",
-		"PAGER_TEMPLATE" => "",
-		"PAGER_DESC_NUMBERING" => "Y",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "Y",
-		"HIDE_NOT_AVAILABLE" => "Y",
-		"OFFERS_CART_PROPERTIES" => "",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"AJAX_OPTION_HISTORY" => "N",
-		"CONVERT_CURRENCY" => "Y",
-		"CURRENCY_ID" => "RUB",
-		"ADD_TO_BASKET_ACTION" => "ADD",
-		"PAGER_BASE_LINK_ENABLE" => "Y",
-		"SHOW_404" => "Y",
-		"MESSAGE_404" => "",
-		"DISABLE_INIT_JS_IN_COMPONENT" => "N",
-		"PAGER_BASE_LINK" => "",
-		"PAGER_PARAMS_NAME" => "arrPager",
-		"COMPONENT_TEMPLATE" => "slider_main",
-		"MESS_BTN_COMPARE" => "Сравнить",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"FILE_404" => ""
-	),
-	false
-);?>
+                "bitrix:catalog.section",
+                "slider_main",
+                array(
+                    "TEMPLATE_THEME" => "blue",
+                    "PRODUCT_DISPLAY_MODE" => "N",
+                    "ADD_PICT_PROP" => "-",
+                    "LABEL_PROP" => "-",
+                    "OFFER_ADD_PICT_PROP" => "FILE",
+                    "OFFER_TREE_PROPS" => array(
+                        0 => "-",
+                    ),
+                    "PRODUCT_SUBSCRIPTION" => "N",
+                    "SHOW_DISCOUNT_PERCENT" => "N",
+                    "SHOW_OLD_PRICE" => "N",
+                    "SHOW_CLOSE_POPUP" => "Y",
+                    "MESS_BTN_BUY" => "Купить",
+                    "MESS_BTN_ADD_TO_BASKET" => "В корзину",
+                    "MESS_BTN_SUBSCRIBE" => "Подписаться",
+                    "MESS_BTN_DETAIL" => "Подробнее",
+                    "MESS_NOT_AVAILABLE" => "Нет в наличии",
+                    "AJAX_MODE" => "Y",
+                    "SEF_MODE" => "N",
+                    "IBLOCK_TYPE" => "services",
+                    "IBLOCK_ID" => "29",
+                    "SECTION_ID" => $_REQUEST["SECTION_ID"],
+                    "SECTION_CODE" => "",
+                    "SECTION_USER_FIELDS" => array(
+                        0 => "",
+                        1 => "",
+                    ),
+                    "ELEMENT_SORT_FIELD" => "sort",
+                    "ELEMENT_SORT_ORDER" => "asc",
+                    "ELEMENT_SORT_FIELD2" => "name",
+                    "ELEMENT_SORT_ORDER2" => "asc",
+                    "FILTER_NAME" => "arrFilter",
+                    "INCLUDE_SUBSECTIONS" => "Y",
+                    "SHOW_ALL_WO_SECTION" => "Y",
+                    "SECTION_URL" => "",
+                    "DETAIL_URL" => "",
+                    "BASKET_URL" => "/personal/basket.php",
+                    "ACTION_VARIABLE" => "action",
+                    "PRODUCT_ID_VARIABLE" => "id",
+                    "PRODUCT_QUANTITY_VARIABLE" => "quantity",
+                    "ADD_PROPERTIES_TO_BASKET" => "Y",
+                    "PRODUCT_PROPS_VARIABLE" => "prop",
+                    "PARTIAL_PRODUCT_PROPERTIES" => "N",
+                    "SECTION_ID_VARIABLE" => "SECTION_ID",
+                    "ADD_SECTIONS_CHAIN" => "Y",
+                    "DISPLAY_COMPARE" => "N",
+                    "SET_TITLE" => "Y",
+                    "SET_BROWSER_TITLE" => "Y",
+                    "BROWSER_TITLE" => "-",
+                    "SET_META_KEYWORDS" => "Y",
+                    "META_KEYWORDS" => "",
+                    "SET_META_DESCRIPTION" => "Y",
+                    "META_DESCRIPTION" => "",
+                    "SET_LAST_MODIFIED" => "Y",
+                    "USE_MAIN_ELEMENT_SECTION" => "Y",
+                    "SET_STATUS_404" => "Y",
+                    "PAGE_ELEMENT_COUNT" => "30",
+                    "LINE_ELEMENT_COUNT" => "3",
+                    "PROPERTY_CODE" => array(
+                        0 => "SPECIAL_OFFER",
+                        1 => "PRICE_BANNER",
+                        2 => "",
+                    ),
+                    "OFFERS_FIELD_CODE" => "",
+                    "OFFERS_PROPERTY_CODE" => "",
+                    "OFFERS_SORT_FIELD" => "sort",
+                    "OFFERS_SORT_ORDER" => "asc",
+                    "OFFERS_SORT_FIELD2" => "active_from",
+                    "OFFERS_SORT_ORDER2" => "desc",
+                    "OFFERS_LIMIT" => "5",
+                    "BACKGROUND_IMAGE" => "-",
+                    "PRICE_CODE" => array(
+                    ),
+                    "USE_PRICE_COUNT" => "Y",
+                    "SHOW_PRICE_COUNT" => "1",
+                    "PRICE_VAT_INCLUDE" => "Y",
+                    "PRODUCT_PROPERTIES" => array(
+                    ),
+                    "USE_PRODUCT_QUANTITY" => "Y",
+                    "CACHE_TYPE" => "A",
+                    "CACHE_TIME" => "36000000",
+                    "CACHE_FILTER" => "Y",
+                    "CACHE_GROUPS" => "Y",
+                    "DISPLAY_TOP_PAGER" => "N",
+                    "DISPLAY_BOTTOM_PAGER" => "Y",
+                    "PAGER_TITLE" => "Товары",
+                    "PAGER_SHOW_ALWAYS" => "Y",
+                    "PAGER_TEMPLATE" => "",
+                    "PAGER_DESC_NUMBERING" => "Y",
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                    "PAGER_SHOW_ALL" => "Y",
+                    "HIDE_NOT_AVAILABLE" => "Y",
+                    "OFFERS_CART_PROPERTIES" => "",
+                    "AJAX_OPTION_JUMP" => "N",
+                    "AJAX_OPTION_STYLE" => "Y",
+                    "AJAX_OPTION_HISTORY" => "N",
+                    "CONVERT_CURRENCY" => "Y",
+                    "CURRENCY_ID" => "RUB",
+                    "ADD_TO_BASKET_ACTION" => "ADD",
+                    "PAGER_BASE_LINK_ENABLE" => "Y",
+                    "SHOW_404" => "Y",
+                    "MESSAGE_404" => "",
+                    "DISABLE_INIT_JS_IN_COMPONENT" => "N",
+                    "PAGER_BASE_LINK" => "",
+                    "PAGER_PARAMS_NAME" => "arrPager",
+                    "COMPONENT_TEMPLATE" => "slider_main",
+                    "MESS_BTN_COMPARE" => "Сравнить",
+                    "AJAX_OPTION_ADDITIONAL" => "",
+                    "FILE_404" => ""
+                ),
+                false
+            );?>
 
         <div class="littleBannersWrap">
             <div id="advantagesBlock">
