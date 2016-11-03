89048d57c81a98eda045b636b3943de40c278e4e
Ruslik <br@webgk.ru>
2016-11-03 17:49:23 +0300

    SUP-5165; adding section list's component on the section pages:

--- local/templates/.default/components/bitrix/catalog/catalog/section.php
+++ local/templates/.default/components/bitrix/catalog/catalog/section.php
@@ -170,7 +170,36 @@
 
 
             <!--elementBlocksWrap-->
-
+            <?$APPLICATION->IncludeComponent(
+	"bitrix:catalog.section.list", 
+	"catalog_section_list", 
+	array(
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"COUNT_ELEMENTS" => "Y",
+		"IBLOCK_ID" => "5",
+		"IBLOCK_TYPE" => "1c_catalog",
+		"SECTION_CODE" => "",
+		"SECTION_FIELDS" => array(
+			0 => "DETAIL_PICTURE",
+			1 => "",
+		),
+		"SECTION_ID" => $_REQUEST["SECTION_ID"],
+		"SECTION_URL" => "",
+		"SECTION_USER_FIELDS" => array(
+			0 => "",
+			1 => "",
+		),
+		"SHOW_PARENT_NAME" => "Y",
+		"TOP_DEPTH" => "2",
+		"VIEW_MODE" => "LINE",
+		"COMPONENT_TEMPLATE" => "catalog_section_list",
+		"HIDE_SECTION_NAME" => "N"
+	),
+	false
+);?>
             <?
                 if (isset($arParams['USE_COMMON_SETTINGS_BASKET_POPUP']) && $arParams['USE_COMMON_SETTINGS_BASKET_POPUP'] == 'Y')
                     $basketAction = (isset($arParams['COMMON_ADD_TO_BASKET_ACTION']) ? $arParams['COMMON_ADD_TO_BASKET_ACTION'] : '');
