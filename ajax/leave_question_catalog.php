38a4e07b99221a27eddd5333c5dfe39230788fcd
Serega <st@webgk.ru>
2016-11-07 18:13:15 +0300

    SUP-5251: edit file

--- ajax/leave_question_catalog.php
+++ ajax/leave_question_catalog.php
@@ -14,11 +14,11 @@ $form_type = $_POST["form_type"];
 $el = new CIBlockElement;
 
 $properties = array(
-	'PRODUCT_ID' => $product_id,
-	'F_EMAIL'    => $email,
-	'PHONE'      => $phone,
-	'COMPANY'    => $company,
-	'TEXT'       => $text
+    'PRODUCT_ID' => $product_id,
+    'F_EMAIL'    => $email,
+    'PHONE'      => $phone,
+    'COMPANY'    => $company,
+    'TEXT'       => $text
 );
 
 $element_fields = Array(
@@ -39,23 +39,23 @@ if ($ar_res = $ar_product->GetNext()) {
 
 if ($element_id > 0) {
 	$mails = array(
-		FORM_FROM_EMAIL,
-		$email
+	    FORM_FROM_EMAIL,
+	    $email
 	);
 	$form_types = getFormTypes();
     $template_fields = array(
-    	"NAME"         => $name,
-    	"PRODUCT_NAME" => $product_name,
-    	"PHONE"        => $phone,
-    	"COMPANY"      => $company,
-    	"TEXT"         => $text,
-    	"PRODUCT_HREF" => $href
+        "NAME"         => $name,
+        "PRODUCT_NAME" => $product_name,
+        "PHONE"        => $phone,
+        "COMPANY"      => $company,
+        "TEXT"         => $text,
+        "PRODUCT_HREF" => $href
 	);
 	foreach ($mails as $mail) {
-		$template_fields['FORM_TYPE'] = $mail == FORM_FROM_EMAIL ? "(" . $form_types[$form_type] . ")" : "";
-		$template_fields['EMAIL'] = $mail;
+        $template_fields['FORM_TYPE'] = $mail == FORM_FROM_EMAIL ? "(" . $form_types[$form_type] . ")" : "";
+        $template_fields['EMAIL'] = $mail;
         CEvent::Send("FORM_QUASTION", SITE_ID, $template_fields, 'N', CARD_QUESTION_FORM_TEMPLATE_ID);
-		CEvent::Send("FORM_QUASTION", SITE_ID, $template_fields, 'N', CARD_QUESTION_FORM_TEMPLATE_ID_EMAIL);
+        CEvent::Send("FORM_QUASTION", SITE_ID, $template_fields, 'N', CARD_QUESTION_FORM_TEMPLATE_ID_EMAIL);
 	}
 }
 ?>
