c6c38d0cb7b0d185be143ed77908aa7c47d7ae6d
Alexandr <as@webgk.ru>
2016-11-14 11:43:08 +0300

    SUP-5362: delete code in component_epilog

--- local/components/bitrix/main.feedback/templates/.default/component_epilog.php
+++ local/components/bitrix/main.feedback/templates/.default/component_epilog.php
@@ -1,15 +1,4 @@
-<?
-if(!defined("B_PROLOG_INCLUDED")||B_PROLOG_INCLUDED!==true)die();
-/**
- * Bitrix vars
- *
- * @var array $arParams
- * @var array $arResult
- * @var CBitrixComponentTemplate $this
- * @global CMain $APPLICATION
- * @global CUser $USER
- */
-?>
+<?if(!defined("B_PROLOG_INCLUDED")||B_PROLOG_INCLUDED!==true)die();?>
 <script>    
 //Форма заказа обратного звонка
 $(document).ready(function(){
