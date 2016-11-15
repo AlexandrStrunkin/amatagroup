69f3a131891a652792f3e633de1dcb73829ea262
Alexandr <as@webgk.ru>
2016-11-15 14:19:48 +0300

    SUP-5259: fix subscribe form, add mail send in form

--- ajax/mailing.php
+++ ajax/mailing.php
@@ -1,51 +1,77 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-CModule::IncludeModule("subscribe");
-// если есть post запрос с почтой то исполняем код
-if($_POST["email"]) {
-    $EMAIL = $_POST["email"];
-    /* получим значение пользователя */
-    if ($USER->IsAuthorized()){
-        global $USER;
-        $USER = $USER->GetID() ;
-    }
-    else {
-       $USER = NULL ;
-    }
-    /* определим рубрики активные рубрики подписок */
-    $RUB_ID = array();
-    $rub = CRubric::GetList(array(), array("ACTIVE"=>"Y"));
-    while($rub->ExtractFields("r_")):
-     $RUB_ID = array($r_ID) ;
-    endwhile;
+    require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+    CModule::IncludeModule("subscribe");
+    // если есть post запрос с почтой то исполняем код
+    if($_POST["email"]) {
+        $EMAIL = $_POST["email"];
+        /* получим значение пользователя */
+        if ($USER->IsAuthorized()) {
+            global $USER;
+            $USER = $USER->GetID() ;
+        }
+        else {
+            $USER = NULL ;
+        }
+        /* определим рубрики активные рубрики подписок */
+        $RUB_ID = array();
+        $rub = CRubric::GetList(array(), array("ACTIVE"=>"Y"));
+        while($rub->ExtractFields("r_")):
+            $RUB_ID = array($r_ID) ;
+            endwhile;
 
-    /* создадим массив на подписку */
-    $subscr = new CSubscription;
-    $arFields = Array(
-        "USER_ID" => $USER,
-        "FORMAT" => "html/text",
-        "EMAIL" => $EMAIL,
-        "ACTIVE" => "Y",
-        "RUB_ID" => $RUB_ID,
-        "SEND_CONFIRM" => "N",
-        'CONFIRMED' => 'Y'
-    );
-    $idsubrscr = $subscr->Add($arFields);
+        /* создадим массив на подписку */
+        $subscr = new CSubscription;
+        $arFields = Array(
+            "USER_ID" => $USER,
+            "FORMAT" => "html/text",
+            "EMAIL" => $EMAIL,
+            "ACTIVE" => "Y",
+            "RUB_ID" => $RUB_ID,
+            "SEND_CONFIRM" => "N",
+            'CONFIRMED' => 'Y'
+        );
+        $idsubrscr = $subscr->Add($arFields);
 
-    if($idsubrscr) {
-      $popuptitle = 'green';
-      $popuptext =  $EMAIL .' успешно подписан на рассылку';
+        if ($idsubrscr) {
+            $popuptitle = 'green';
+            $popuptext = $EMAIL .' успешно подписан на рассылку';
+            
+            //отправляем письмо с уведомлением о том что пользователь добавлен в подписки
+            if (empty($arFields["USER_ID"])) {
+                $arEventFields = array(
+                    "MESSAGE" => 'Добавлен анонимный подписчик на рассылку',
+                    "EMAIL" => $arFields["EMAIL"],                   
+                );        
+            } else {
+                $rsUser = CUser::GetByID($arFields["USER_ID"]);
+                $arUser = $rsUser->Fetch();
+                if (!empty($arUser["NAME"])) {
+                    $mailName = 'Имя пользователя: '.$arUser["NAME"];     
+                }
+                if (!empty($arUser["LAST_NAME"])) {
+                    $mailLastName = 'Фамилия пользователя: '.$arUser["LAST_NAME"];     
+                }
+                $arEventFields = array(
+                    "MESSAGE" => 'Добавлен подписчик на рассылку',
+                    "EMAIL" => $arFields["EMAIL"],
+                    "USER_ID" => 'ID пользователя:'.$arFields["USER_ID"],
+                    "NAME" => $mailName,
+                    "LAST_NAME" => $mailLastName,                 
+                );
+            }            
+            CEvent::Send ("NEW_SUBSCRIBE", "s1", $arEventFields, "N", 90);
+            
+        }
+        else {
+            $popuptitle = 'red';
+            $popuptext = $EMAIL .' уже подписан или неправильно набран';
+        }
+        $ajax_color = $popuptitle;
+        /* если ajax не подключен */
+        if ($_POST["action"] != "ajax") {
+            header('Location: '.$_SERVER['HTTP_REFERER']);
+        }
     }
-    else {
-      $popuptitle = 'red';
-      $popuptext =   $EMAIL .' уже был подписан на рассылку';
-    }
-    $ajax_color = $popuptitle;
-    /* если ajax не подключен */
-    if ($_POST["action"] != "ajax") {
-       header('Location: '.$_SERVER['HTTP_REFERER']);
-    }
-}
 
 ?>
 
