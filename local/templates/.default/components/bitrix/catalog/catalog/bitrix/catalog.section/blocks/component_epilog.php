fcf65a85473e26e77f39393f6538c80d9dcdc61b
Mikhail <lm@webgk.ru>
2016-11-19 18:47:21 +0300

    SUP-5390: New layout for blocks with overflowed names

--- local/templates/.default/components/bitrix/catalog/catalog/bitrix/catalog.section/blocks/component_epilog.php
+++ local/templates/.default/components/bitrix/catalog/catalog/bitrix/catalog.section/blocks/component_epilog.php
@@ -3,6 +3,49 @@
 /** @var @global CMain $APPLICATION */
 use Bitrix\Main\Loader;
 global $APPLICATION;
+$APPLICATION->AddHeadScript(DEFAULT_TEMPLATE_PATH . "/js/clamp.js");?>
+<script>
+	$(document).ready(function(){
+		var products_name = document.querySelectorAll(".productName");
+		Array.prototype.forEach.call(products_name, function(product, i){
+			$clamp(product, {clamp: 2});
+		});
+		// показываем скрытую часть названия в отображении блоками
+		$(".productWrapper").hover(
+			function(){
+				var outer_block = $(this).parent(), // внешний блок, его высота эталонная
+					name_block = $(this).find(".productName"), // блок названия
+					additional_height = name_block[0].scrollHeight - name_block.height(); // добавляемая скрытая разница в высоте
+			
+				$(this).css({
+					"height" : outer_block.height() + additional_height + "px",
+					"z-index": 2,
+					"border-bottom" : "1px solid #e6e6e6"
+				});
+				name_block.css({
+					"display" : "block",
+					"overflow" : "visible"
+				});
+			},
+			function(){
+				var outer_block = $(this).parent(),
+					name_block = $(this).find(".productName"),
+					additional_height = name_block[0].scrollHeight - name_block.height();
+			
+				$(this).css({
+					"height" : outer_block.height() + "px",
+					"z-index": 0,
+					"border-bottom" : "none"
+				});
+				name_block.css({
+					"display" : "-webkit-box",
+					"overflow" : "hidden"
+				});
+			}
+		)
+	})
+</script>
+<?
 if (isset($templateData['TEMPLATE_THEME']))
 {
 	$APPLICATION->SetAdditionalCSS($templateData['TEMPLATE_THEME']);
