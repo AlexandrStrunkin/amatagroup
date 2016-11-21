666146c6bb106490c61b15ca9210c58e36555270
Serega <st@webgk.ru>
2016-11-21 14:57:14 +0300

    SUP-5434: the addition of the term aktivnoysti shares

--- local/templates/.default/components/bitrix/news/news_without_tabs/bitrix/news.detail/.default/template.php
+++ local/templates/.default/components/bitrix/news/news_without_tabs/bitrix/news.detail/.default/template.php
@@ -2,6 +2,24 @@
 
 <div class="detail_news">
     <div class="date"><?= date_format(date_create_from_format('d.m.Y H:i:s', $arResult['DATE_CREATE']), 'd.m.Y') ?></div><br>
+    <div class="<?= (date_format(date_create_from_format('d.m.Y H:i:s', $arResult['DATE_ACTIVE_TO']), 'd.m.Y') > date('d.m.Y') || !$arItem['DATE_ACTIVE_TO']) ? 'green' : 'red'; ?>"></div>
+    <span>
+    <?$datetime_from = new DateTime(date_format(date_create_from_format('d.m.Y H:i:s', $arResult['DATE_ACTIVE_FROM']), 'd.m.Y'));
+        $datetime2 = new DateTime(date_format(date_create_from_format('d.m.Y H:i:s', $arResult['DATE_ACTIVE_TO']), 'd.m.Y'));
+        $interval_validity = $datetime_from->diff($datetime2);
+        $format_day_validity = $interval_validity->format('%d дней');
+        echo 'Срок действия акции ' . getNumEnding($format_day_validity, array("день", "дня", "дней"));?>
+    </span>
+    <span class="date_validity">
+    <?
+        $datetime_to = new DateTime(date('d.m.Y'));
+        $interval_deadline = $datetime2->diff($datetime_to);
+        $format_day = $interval_deadline->format('%d');
+        if( date_format(date_create_from_format('d.m.Y H:i:s', $arResult['DATE_ACTIVE_TO']), 'd.m.Y') > date('d.m.Y')){
+            echo 'до конца акции осталось ' . getNumEnding($format_day, array("день", "дня", "дней"));
+        }
+    ?>
+    </span><br><br>
 	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
 		<img class="detail_picture" border="0" src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>" width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>" height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>" alt="<?=$arResult["NAME"]?>"  title="<?=$arResult["NAME"]?>" />
 	<?endif?>
