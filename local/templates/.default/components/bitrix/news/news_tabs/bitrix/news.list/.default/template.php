1de71b8cd0be02b2b8f85ed03fc46ca68e45db04
Alexandr <as@webgk.ru>
2016-11-24 12:38:44 +0300

    SUP-5460: fix date in news list

--- local/templates/.default/components/bitrix/news/news_tabs/bitrix/news.list/.default/template.php
+++ local/templates/.default/components/bitrix/news/news_tabs/bitrix/news.list/.default/template.php
@@ -13,11 +13,18 @@
     <?
 	$this->AddEditAction($item['ID'], $item['EDIT_LINK'], CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_EDIT"));
 	$this->AddDeleteAction($item['ID'], $item['DELETE_LINK'], CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('NEWS_ELEMENT_DELETE_CONFIRM')));
-    ?>
+    /*arshow($item);*/?>
         <li id="<?=$this->GetEditAreaId($item['ID']);?>">
             <div class="imageWrapper"><img src="<?= $item['PREVIEW_PICTURE']['SRC'] ?>" alt="<?= $item['NAME'] ?>"/></div>
             <div class="tabs_news_list_text">
-            	<p class="dateText"><?= date_format(date_create_from_format('d.m.Y H:i:s', $item['DATE_CREATE']), 'd.m.Y') ?></p>
+                <?
+                    if (!empty($item['ACTIVE_FROM'])) {
+                        $news_date = $item['ACTIVE_FROM'];
+                    } else {
+                        $news_date = date_format(date_create_from_format('d.m.Y H:i:s', $item['DATE_CREATE']), 'd.m.Y');
+                    }
+                ?>
+            	<p class="dateText"><?= $news_date ?></p>
             	<a href="<?= $item['DETAIL_PAGE_URL'] ?>" class="newsName"><?= $item['NAME'] ?></a>
             	<p class="newsText">
             		<?= $item['PREVIEW_TEXT'] ?>
