35d9f58497d5689f1ca168c5e08e5179e16e9a9e
Serega <st@webgk.ru>
2016-11-28 18:24:00 +0300

    SUP-5476: edit detail page

--- local/templates/.default/components/bitrix/news/news_without_tabs/bitrix/news.detail/.default/template.php
+++ local/templates/.default/components/bitrix/news/news_without_tabs/bitrix/news.detail/.default/template.php
@@ -4,29 +4,12 @@
 
     <span>
     <?  // записываем даты создания, начала активности и окончания в переменные в формате d.m.Y
-        $datetime_from = date_format(date_create_from_format('d.m.Y H:i:s', $arResult['DATE_ACTIVE_FROM']), 'd.m.Y');
-        $datetime_to = date_format(date_create_from_format('d.m.Y H:i:s', $arResult['DATE_ACTIVE_TO']), 'd.m.Y');
         $datetime_create = date_format(date_create_from_format('d.m.Y H:i:s', $arResult['DATE_CREATE']), 'd.m.Y');
-        $date_today = new DateTime(date('d.m.Y'));
-        $date_to = new DateTime(date_format(date_create_from_format('d.m.Y H:i:s', $arResult['DATE_ACTIVE_TO']), 'd.m.Y'));
-        //подсчитываем разницу между датами и выводим количество дней
-        $interval_deadline = $date_to->diff($date_today);
-        $format_day = $interval_deadline->format('%d');
+
     ?>
     </span>
-     <div class="<?= ($datetime_to > date('d.m.Y') || !$arResult['DATE_ACTIVE_TO']) ? 'green' : 'red'; ?>">
-            <? if ($datetime_to > date('d.m.Y') || !$arResult['DATE_ACTIVE_TO']) { ?>
-                <span class="date_validity">
-                    <?= GetMessage('DEADLINE') . getNumEnding($format_day, array(GetMessage('DAY_1'), GetMessage('DAY_2'), GetMessage('DAY_3')));?>
-                </span>
-            <?} else { ?>
-                <span class="date_validity">
-                    <?= GetMessage('DEADLINE_CLOSE');?>
-                </span>
-                <?}?>
-            </div><br><br>
         <p class="dateText">
-            <?= GetMessage('VALIDITY') . $datetime_create . GetMessage('VALIDITY_2') . $datetime_to; ?>
+            <?= $datetime_create ; ?>
         </p>
     </span>
 	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
