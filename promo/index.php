9db1d75e44201ceaee4f752d313c8ee610b6f03f
Serega <st@webgk.ru>
2016-11-28 14:43:06 +0300

    SUP-5478: copy template new wrap

--- promo/index.php
+++ promo/index.php
@@ -3,8 +3,8 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Акции");
 ?>
 <?$APPLICATION->IncludeComponent(
-	"bitrix:news", 
-	"news_without_tabs", 
+	"bitrix:news",
+	"news_without_tabs",
 	array(
 		"ADD_ELEMENT_CHAIN" => "Y",
 		"ADD_SECTIONS_CHAIN" => "N",
@@ -101,4 +101,134 @@ $APPLICATION->SetTitle("Акции");
 	),
 	false
 );?>
+
+<div class="backgroundColor">
+    <!--widthWrapper-->
+    <div class="widthWrapper">
+        <!--productBlockWrapper-->
+        <div class="productBlockWrapper">
+            <div class="newsBlock">
+            <?
+                  global $ShowWithImage;
+                  $curr_date = date('U');
+                  $date_create_date = $curr_date - (86400 * NEW_PRODUCT_STATUS_LENGTH);
+                  $ShowWithImage = array(
+                        ">=DATE_CREATE" => ConvertTimeStamp($date_create_date,"FULL"),
+                        '!PREVIEW_PICTURE' => false
+                    );
+                ?>
+                <?$APPLICATION->IncludeComponent("bitrix:catalog.section", "product_news_promo", Array(
+	"ACTION_VARIABLE" => "action",	// Название переменной, в которой передается действие
+		"ADD_PICT_PROP" => "-",	// Дополнительная картинка основного товара
+		"ADD_PROPERTIES_TO_BASKET" => "Y",	// Добавлять в корзину свойства товаров и предложений
+		"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+		"ADD_TO_BASKET_ACTION" => "ADD",	// Показывать кнопку добавления в корзину или покупки
+		"AJAX_MODE" => "N",	// Включить режим AJAX
+		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+		"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+		"BACKGROUND_IMAGE" => "-",	// Установить фоновую картинку для шаблона из свойства
+		"BASKET_URL" => "/personal/basket.php",	// URL, ведущий на страницу с корзиной покупателя
+		"BROWSER_TITLE" => "-",	// Установить заголовок окна браузера из свойства
+		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
+		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+		"CACHE_TYPE" => "A",	// Тип кеширования
+		"CONVERT_CURRENCY" => "N",	// Показывать цены в одной валюте
+		"DETAIL_URL" => "/catalog/#ELEMENT_CODE#/",	// URL, ведущий на страницу с содержимым элемента раздела
+		"DISABLE_INIT_JS_IN_COMPONENT" => "Y",	// Не подключать js-библиотеки в компоненте
+		"DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
+		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+		"ELEMENT_SORT_FIELD" => "rand",	// По какому полю сортируем элементы
+		"ELEMENT_SORT_FIELD2" => "id",	// Поле для второй сортировки элементов
+		"ELEMENT_SORT_ORDER" => "date_create",	// Порядок сортировки элементов
+		"ELEMENT_SORT_ORDER2" => "desc",	// Порядок второй сортировки элементов
+		"FILTER_NAME" => "ShowWithImage",	// Имя массива со значениями фильтра для фильтрации элементов
+		"HIDE_NOT_AVAILABLE" => "Y",	// Товары, не доступные для покупки
+		"IBLOCK_ID" => "5",	// Инфоблок
+		"IBLOCK_TYPE" => "1c_catalog",	// Тип инфоблока
+		"INCLUDE_SUBSECTIONS" => "Y",	// Показывать элементы подразделов раздела
+		"LABEL_PROP" => "-",	// Свойство меток товара
+		"LINE_ELEMENT_COUNT" => "4",	// Количество элементов выводимых в одной строке таблицы
+		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+		"MESS_BTN_ADD_TO_BASKET" => "В корзину",	// Текст кнопки "Добавить в корзину"
+		"MESS_BTN_BUY" => "Купить",	// Текст кнопки "Купить"
+		"MESS_BTN_DETAIL" => "Подробнее",	// Текст кнопки "Подробнее"
+		"MESS_BTN_SUBSCRIBE" => "Подписаться",	// Текст кнопки "Уведомить о поступлении"
+		"MESS_NOT_AVAILABLE" => "Нет в наличии",	// Сообщение об отсутствии товара
+		"META_DESCRIPTION" => "-",	// Установить описание страницы из свойства
+		"META_KEYWORDS" => "-",	// Установить ключевые слова страницы из свойства
+		"OFFERS_CART_PROPERTIES" => "",	// Свойства предложений, добавляемые в корзину
+		"OFFERS_FIELD_CODE" => array(	// Поля предложений
+			0 => "",
+			1 => "",
+		),
+		"OFFERS_LIMIT" => "15",	// Максимальное количество предложений для показа (0 - все)
+		"OFFERS_PROPERTY_CODE" => array(	// Свойства предложений
+			0 => "",
+			1 => "",
+		),
+		"OFFERS_SORT_FIELD" => "rand",	// По какому полю сортируем предложения товара
+		"OFFERS_SORT_FIELD2" => "id",	// Поле для второй сортировки предложений товара
+		"OFFERS_SORT_ORDER" => "asc",	// Порядок сортировки предложений товара
+		"OFFERS_SORT_ORDER2" => "desc",	// Порядок второй сортировки предложений товара
+		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+		"PAGER_TITLE" => "Товары",	// Название категорий
+		"PAGE_ELEMENT_COUNT" => "8",	// Количество элементов на странице
+		"PARTIAL_PRODUCT_PROPERTIES" => "N",	// Разрешить добавлять в корзину товары, у которых заполнены не все характеристики
+		"PRICE_CODE" => array(	// Тип цены
+			0 => "Оптовая 1 Для сайта",
+		),
+		"PRICE_VAT_INCLUDE" => "Y",	// Включать НДС в цену
+		"PRODUCT_DISPLAY_MODE" => "N",	// Схема отображения
+		"PRODUCT_ID_VARIABLE" => "id",	// Название переменной, в которой передается код товара для покупки
+		"PRODUCT_PROPERTIES" => "",	// Характеристики товара
+		"PRODUCT_PROPS_VARIABLE" => "prop",	// Название переменной, в которой передаются характеристики товара
+		"PRODUCT_QUANTITY_VARIABLE" => "",	// Название переменной, в которой передается количество товара
+		"PRODUCT_SUBSCRIPTION" => "N",	// Разрешить оповещения для отсутствующих товаров
+		"PROPERTY_CODE" => array(	// Свойства
+			0 => "",
+			1 => "",
+		),
+		"SECTION_CODE" => "",	// Код раздела
+		"SECTION_CODE_PATH" => "",	// Путь из символьных кодов раздела
+		"SECTION_ID" => $_REQUEST["SECTION_ID"],	// ID раздела
+		"SECTION_ID_VARIABLE" => "SECTION_ID",	// Название переменной, в которой передается код группы
+		"SECTION_URL" => "/catalog/news_product/",	// URL, ведущий на страницу с содержимым раздела
+		"SECTION_USER_FIELDS" => array(	// Свойства раздела
+			0 => "",
+			1 => "",
+		),
+		"SEF_MODE" => "Y",	// Включить поддержку ЧПУ
+		"SEF_RULE" => "",	// Правило для обработки
+		"SET_BROWSER_TITLE" => "Y",	// Устанавливать заголовок окна браузера
+		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+		"SET_META_DESCRIPTION" => "Y",	// Устанавливать описание страницы
+		"SET_META_KEYWORDS" => "Y",	// Устанавливать ключевые слова страницы
+		"SET_STATUS_404" => "N",	// Устанавливать статус 404
+		"SET_TITLE" => "Y",	// Устанавливать заголовок страницы
+		"SHOW_404" => "N",	// Показ специальной страницы
+		"SHOW_ALL_WO_SECTION" => "Y",	// Показывать все элементы, если не указан раздел
+		"SHOW_CLOSE_POPUP" => "N",	// Показывать кнопку продолжения покупок во всплывающих окнах
+		"SHOW_DISCOUNT_PERCENT" => "N",	// Показывать процент скидки
+		"SHOW_OLD_PRICE" => "N",	// Показывать старую цену
+		"SHOW_PRICE_COUNT" => "1",	// Выводить цены для количества
+		"TEMPLATE_THEME" => "blue",	// Цветовая тема
+		"USE_MAIN_ELEMENT_SECTION" => "N",	// Использовать основной раздел для показа элемента
+		"USE_PRICE_COUNT" => "N",	// Использовать вывод цен с диапазонами
+		"USE_PRODUCT_QUANTITY" => "N",	// Разрешить указание количества товара
+		"COMPONENT_TEMPLATE" => "product_news"
+	),
+	false
+);?>
+            </div>
+        </div>
+    </div>
+</div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
