8768d75ef8cfa5168ac32f16df70fc11684ac083
Ruslik <br@webgk.ru>
2016-12-12 16:30:28 +0300

    SUP-5579: fixing disposition of Yandex.Metrika code

--- local/templates/pages_with_map/header.php
+++ local/templates/pages_with_map/header.php
@@ -12,43 +12,7 @@
 </head>
 <body class="<?if ($curPage == SITE_DIR."index.php"){?>mainPage<?}?>">
 <div id="panel"><?$APPLICATION->ShowPanel();?></div>
-<!-- Yandex.Metrika counter --> 
-<script type="text/javascript"> 
-(function (d, w, c) { 
-    (w[c] = w[c] || []).push(function() { 
-        try { 
-            w.yaCounter38954910 = new Ya.Metrika({ 
-                id:38954910, 
-                clickmap:true, 
-                trackLinks:true, 
-                accurateTrackBounce:true, 
-                webvisor:true, 
-                trackHash:true, 
-                ecommerce:"dataLayer" 
-            }); 
-        } 
-        catch(e) { } 
-    }); 
-    var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { 
-        n.parentNode.insertBefore(s, n); 
-    }; 
-    s.type = "text/javascript"; 
-    s.async = true; 
-    s.src = "https://mc.yandex.ru/metrika/watch.js"; 
-    if (w.opera == "[object Opera]") { 
-        d.addEventListener("DOMContentLoaded", f, false); 
-    } else { 
-        f(); 
-    } 
-})
-(document, window, "yandex_metrika_callbacks"); 
-</script> 
-<noscript>
-    <div>
-        <img src="https://mc.yandex.ru/watch/38954910" style="position:absolute; left:-9999px;" alt="" />
-    </div>
-</noscript> 
-<!-- /Yandex.Metrika counter -->
+
 <?include($_SERVER["DOCUMENT_ROOT"].DEFAULT_TEMPLATE_PATH."/include/header.php")?>
 
 <!--main-->
